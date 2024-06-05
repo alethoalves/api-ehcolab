debdb96bde0f0f52e5823a7643b694911fa64b09
Aletho Alves (PC COPOL) <alethoalves@gmail.com>
2024-06-05 07:34:45 -0300

    Deletando a pasta app

--- src/app/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as auth from "../app/controllers/authController.js.js";
-const router = Router();
-
-router.post('/signin', auth.signin);
-router.post('/signup', auth.signup);
-
-
-export default router;
--- src/app/routes/privateRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as auth from "../app/controllers/authController.js.js";
-import { privateRoute } from "../app/middlewares/private.js.js";
-const router = Router();
-
-router.get('/isLogged',privateRoute);
-
-
-export default router;
